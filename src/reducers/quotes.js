09fa049b998dbad4b839d5d1701c568e9d046011
flatironschool <danliu277@gmail.com>
2020-03-06 16:14:16 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,24 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [ ...state, action.quote] 
+    case 'REMOVE_QUOTE':
+      return [ ...state.filter(quote => quote.id !== action.quoteId) ]
+    case 'UPVOTE_QUOTE':
+      return [ ...state.map(quote => {
+        if(quote.id === action.quoteId) {
+          quote.votes++
+        }
+        return quote
+      }) ]
+    case 'DOWNVOTE_QUOTE':
+      return [ ...state.map(quote => {
+        if(quote.id === action.quoteId && quote.votes > 0) {
+          quote.votes--
+        }
+        return quote
+      }) ]
+    default:
+      return state;
+  }
 }
